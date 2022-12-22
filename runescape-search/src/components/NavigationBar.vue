0e11e5ebdfcb0ab7814295ec9d1b55100c45f639
Sinan <haxsmith77@gmail.com>
2022-12-22 11:54:22 +1100

    Updated all

--- runescape-search/src/components/NavigationBar.vue
+++ runescape-search/src/components/NavigationBar.vue
@@ -8,7 +8,7 @@
       <v-toolbar-items>
         <v-btn variant="tonal" :to="{ name: 'home' }">Home</v-btn>
         <v-btn variant="tonal" :to="{ name: 'about' }">About</v-btn>
-        <!-- <v-btn variant="tonal" :to="{ name: 'News' }">News</v-btn> -->
+        <v-btn variant="tonal" :to="{ name: 'news' }">News</v-btn>
       </v-toolbar-items>
 
       <v-spacer></v-spacer>
